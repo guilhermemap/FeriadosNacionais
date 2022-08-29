64195710b3edcddee56eae595590bbe8b8329853
guilhermemap <guilherme@acheipneus.com.br>
2022-08-29 11:07:53 -0300

    anotações no final do CSV => comentários com #

--- FeriadosNacionais/DiasUteis.cs
+++ FeriadosNacionais/DiasUteis.cs
@@ -1,5 +1,33 @@
-﻿namespace FeriadosNacionais;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+
+namespace FeriadosNacionais;
 public static class DiasUteis
 {
-
+    static readonly List<CsvFeriado> Feriados = LerCsv();
+    static List<CsvFeriado> LerCsv(string csv_path = "feriados_nacionais.csv")
+    {
+        var csvConfig = new CsvConfiguration(CultureInfo.GetCultureInfo("en-US"))
+        {
+            HasHeaderRecord = true,
+            Delimiter = ",",
+            Encoding = System.Text.Encoding.UTF8,
+            BadDataFound = null,
+            AllowComments = true,
+            Comment = '#',
+        };
+        using var streamReader = new StreamReader(csv_path);
+        using var csvReader = new CsvReader(streamReader, csvConfig);
+        var csv_orders = csvReader.GetRecords<CsvFeriado>();
+        return csv_orders.ToList();
+    }
+    public static bool IsFeriado(DateTime date)
+    {
+        return Feriados.Find(x => x.Data.Date == date.Date) != null;
+    }
+    public static bool IsDiaUtil(DateTime date)
+    {
+        return Feriados.Find(x => x.Data.Date == date.Date) == null;
+    }
 }
