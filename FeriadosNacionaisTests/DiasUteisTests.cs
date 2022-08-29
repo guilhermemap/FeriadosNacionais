d6b727a99a3336dfb948deabcb124486173b7cdf
guilhermemap <guilherme@acheipneus.com.br>
2022-08-29 11:07:58 -0300

    tests

---
+++ FeriadosNacionaisTests/DiasUteisTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FeriadosNacionais;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeriadosNacionais.Tests;
+
+[TestClass()]
+public class DiasUteisTests
+{
+    [TestMethod()]
+    public void IsFeriadoTest()
+    {
+        var date = DateTime.Parse("2022/08/28");
+        Assert.IsTrue(DiasUteis.IsDiaUtil(date));
+        var date2 = DateTime.Parse("2022/09/07");
+        Assert.IsFalse(DiasUteis.IsDiaUtil(date2));
+    }
+}
